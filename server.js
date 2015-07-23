bcba64ad5a874c53ad5682aba1c3e954fd66ab68
andytrail <dbailey2006@gmail.com>
2015-07-23 10:21:48 -0700

    adding foo.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function db(req, res) {
-res.writeHead(200, {'Content-Type': 'text/plain' });
-res.end('Hello World\n');
-}).listen(3000, "127.0.0.1");
-console.log('Server running at this place');
+res.writeHead(200, {'Content-Type': 'html' });
+res.end('<br><br><br><br><br><br><b>Hello World,<b>\n');
+}).listen(3001, "127.0.0.1");
+console.log('Brogramming in this place with node.js');
